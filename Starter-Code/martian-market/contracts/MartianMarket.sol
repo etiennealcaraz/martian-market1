ce52102904ad5187e52a38ec87990ab29968bb38
etiennealcaraz <70927754+etiennealcaraz@users.noreply.github.com>
2021-02-28 18:53:37 -0800

    Update MartianMarket.sol

--- Starter-Code/martian-market/contracts/MartianMarket.sol
+++ Starter-Code/martian-market/contracts/MartianMarket.sol
@@ -12,7 +12,7 @@ contract MartianMarket is ERC721, Ownable {
 
     mapping(uint => MartianAuction) public auctions;
 
-    function registerLand(string memory uri) public payable onlyOwner {
+    function registerLand(string memory tokenURI) public payable onlyOwner {
         uint _id = totalSupply();
         _mint(msg.sender, _id);
         _setTokenURI(_id, tokenURI);
@@ -23,29 +23,43 @@ contract MartianMarket is ERC721, Ownable {
         auctions[tokenId] = new MartianAuction(foundation_address);
     }
 
-    function endAuction(uint tokenId) public onlyOwner landRegistered(tokenId) {
-        MartianAuction auction = auctions[tokenId];
+    function endAuction(uint tokenId) public onlyOwner {
+        require(_exists(tokenId), "Land not registered!");
+        MartianAuction auction = getAuction(tokenId);
         auction.auctionEnd();
         safeTransferFrom(owner(), auction.highestBidder(), tokenId);
     }
-
-    function auctionEnded(uint tokenId) public view returns(bool) {
-        MartianAuction auction = auctions[tokenId];
-        return auction.ended();
+    
+    f    function getAuction(uint tokenId) public view returns(MartianAuction auction) {
+        // Pull information from createAuction(uint tokenID)
+        return auctions[tokenId];
     }
 
-    function highestBid(uint tokenId) public view landRegistered(tokenId) returns(uint) {
-        MartianAuction auction = auctions[tokenId];
+    function auctionEnded(uint tokenId) public view returns(bool) {
+       // Require existing tokenID to prevent lossing ether 
+        require(_exists(tokenId), "Unregistered land. Please double check your token ID.");    
+        MartianAuction auction = getAuction(tokenId);
+        return auction.ended();
+     }
+
+    function highestBid(uint tokenId) public view returns(uint) {
+       // Require existing tokenID to prevent lossing ether 
+        require(_exists(tokenId), "Unregistered land. Please double check your token ID.");    
+        MartianAuction auction = getAuction(tokenId);
         return auction.highestBid();
     }
 
-    function pendingReturn(uint tokenId, address sender) public view landRegistered(tokenId) returns(uint) {
-        MartianAuction auction = auctions[tokenId];
+    function pendingReturn(uint tokenId, address sender) public view returns(uint) {
+       // Require existing tokenID to prevent lossing ether 
+        require(_exists(tokenId), "Unregistered land. Please double check your token ID.");    
+        MartianAuction auction = getAuction(tokenId);
         return auction.pendingReturn(sender);
-    }
+     }
 
-    function bid(uint tokenId) public payable landRegistered(tokenId) {
-        MartianAuction auction = auctions[tokenId];
+    function bid(uint tokenId) public payable {
+       // Require existing tokenID to prevent lossing ether 
+        require(_exists(tokenId), "Unregistered land. Please double check your token ID.");    
+        MartianAuction auction = getAuction(tokenId);
         auction.bid.value(msg.value)(msg.sender);
     }
 
