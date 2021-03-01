06dd20b7d4d00ad3ee7cb562924efc9aa9bc44ef
etiennealcaraz <70927754+etiennealcaraz@users.noreply.github.com>
2021-02-28 17:53:32 -0800

    Update truffle-config.js

--- Starter-Code/martian-market/truffle-config.js
+++ Starter-Code/martian-market/truffle-config.js
@@ -89,7 +89,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "pragma solidity >=0.4.22 <0.6.0",    // Fetch exact version from solc-bin (default: truffle's version)
+       version: "pragma solidity >=0.4.22 <0.6.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
