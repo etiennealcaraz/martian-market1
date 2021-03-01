2b912d6e844b37f5c2758d2c4d347e522ff3b1c7
etiennealcaraz <70927754+etiennealcaraz@users.noreply.github.com>
2021-02-28 19:29:11 -0800

    Update dapp.js

--- Starter-Code/martian-market/dapp.js
+++ Starter-Code/martian-market/dapp.js
@@ -3,7 +3,7 @@ import auctionJson from "./MartianAuction.json";
 
 const Web3 = require("web3");
 
-const contractAddress = ""; // insert MartianMarket contract address here
+const contractAddress = "0x366695C3b5668e8Ff3548e5Ff6da6b43A40d3E0f"; // insert MartianMarket contract address here
 
 const dApp = {
   ethEnabled: function() {
